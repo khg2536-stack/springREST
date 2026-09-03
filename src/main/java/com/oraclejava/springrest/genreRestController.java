e5548f808979dc15f56e4d8ad2c82e4c52842c84
khg2536 <khg2536@gmail.com>
2026-09-03 17:30:49 +0900

    장르 컨트롤러

---
+++ src/main/java/com/oraclejava/springrest/genreRestController.java
@@ -0,0 +1,22 @@
+package com.oraclejava.springrest;
+
+import com.oraclejava.springrest.models.Genre;
+import com.oraclejava.springrest.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/genres")
+public class genreRestController {
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @GetMapping
+    public List<Genre> getGenres(){
+        return genreRepository.findAll();
+    }
+}
